a13faf8ad6cee4a9178b02023514df304e04a0d3
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2020-06-20 21:42:10 +0100

    Added core module to the project

---
+++ core/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform") version "1.3.72"
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+kotlin {
+    jvm()
+    js()
+    iosArm32()
+    iosArm64()
+    iosX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-commons"))
+                implementation(kotlin("test-annotations"))
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,5 @@
  */
 
 rootProject.name = "Venture"
+
+include(":core")
