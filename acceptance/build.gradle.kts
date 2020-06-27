6968478341c7df39dd701e88e789e8c52af4f8c8
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2020-06-27 11:00:48 +0100

    Ignoring tests failures in Gradle
    The test failures are ignored by Gradle so that they can be reported by
    GitHub itself.

--- acceptance/build.gradle.kts
+++ acceptance/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     kotlin("jvm") version "1.3.72"
 }
 
+tasks {
+    test {
+        ignoreFailures = true
+    }
+}
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("io.cucumber:cucumber-java8:6.1.1")
