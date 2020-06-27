d4e1f06c820162488077506cbae765c6ff7e3594
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2020-06-27 11:05:36 +0100

    Reverted change to prevent gradle to fail the build when the tests fail.

--- acceptance/build.gradle.kts
+++ acceptance/build.gradle.kts
@@ -2,12 +2,6 @@ plugins {
     kotlin("jvm") version "1.3.72"
 }
 
-tasks {
-    test {
-        ignoreFailures = true
-    }
-}
-
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("io.cucumber:cucumber-java8:6.1.1")
