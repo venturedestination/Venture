d1a9336f152da8d228684276f6e000237c506763
Pablo Sánchez <pablo.sanchez.alonso@gmail.com>
2020-06-27 11:34:19 +0100

    Removed false failure.

--- acceptance/src/test/kotlin/StepDefinitions.kt
+++ acceptance/src/test/kotlin/StepDefinitions.kt
@@ -114,7 +114,6 @@ class StepDefinitions : En {
         }
 
         Then("I get a confirmation that my profile picture has been set") {
-            throw Exception("This pipeline will fail")
         }
     }
 }
